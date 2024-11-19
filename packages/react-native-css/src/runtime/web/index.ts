91d5aa9e2d73c7f89cd0cc180efaa4a219ef55fb
Mark Lawlor <mwlawlor@gmail.com>
2024-11-19 16:38:57 +1000

    Add jsx

--- packages/react-native-css/src/runtime/web/index.ts
+++ packages/react-native-css/src/runtime/web/index.ts
@@ -1,1 +1,8 @@
+import type { JSXFunction } from "../runtime.types";
+
 export type * from "../runtime.types";
+
+export const interopComponents = new Map<
+  object | string,
+  Parameters<JSXFunction>[0]
+>();
