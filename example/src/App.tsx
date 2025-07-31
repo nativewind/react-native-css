a3114a2d070d573032b6752e99fec9dbf4134789
marklawlor <3946701+marklawlor@users.noreply.github.com>
2025-07-31 13:13:25 +0000

    fix: line-height parsing (#27)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,9 +6,7 @@ export default function App() {
   return (
     <>
       <View className="justify-center items-center h-full">
-        <Text className="text-green-500 ring-2 ring-blue-500 shadow-xl shadow-red-500">
-          Test Component
-        </Text>
+        <Text className="text-red-500">Test Component</Text>
       </View>
     </>
   );
