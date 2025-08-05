4ad72eaa8bf7ede8aef1e165dd6a91ad3b52ef00
Mark Lawlor <mwlawlor@gmail.com>
2025-08-05 10:44:18 +1000

    chore: enable eas build cache provider

---
+++ example/app.config.ts
@@ -0,0 +1,24 @@
+import type { ConfigContext, ExpoConfig } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  return {
+    ...config,
+    name: "example",
+    slug: "example",
+    plugins: ["expo-dev-client"],
+    userInterfaceStyle: "automatic",
+    android: {
+      package: "dev.reactnativecss",
+    },
+    ios: {
+      bundleIdentifier: "dev.reactnativecss",
+    },
+    experiments: {
+      reactCompiler: false,
+      buildCacheProvider:
+        process.env.CI || process.env.EAS_BUILD_CACHE_PROVIDER
+          ? "eas"
+          : undefined,
+    },
+  };
+};
