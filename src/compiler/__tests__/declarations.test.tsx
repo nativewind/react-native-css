d8cb1bc7fe0e1076739063bc3e305e678b27d512
Mark Lawlor <mwlawlor@gmail.com>
2025-08-16 06:57:33 +1000

    chore: add declarations.test.tsx

---
+++ src/compiler/__tests__/declarations.test.tsx
@@ -0,0 +1,21 @@
+import { compile } from "../compiler";
+
+const tests = [
+  ["stroke: black;", [{ d: [["#000", ["stroke"]]], s: [1, 1] }]],
+] as const;
+
+test.each(tests)("declarations for %s", (declarations, expected) => {
+  const compiled = compile(`.my-class { ${declarations} }`);
+
+  const stylesheet = compiled.stylesheet();
+
+  const myClassDeclarations = stylesheet.s?.find(
+    (rule) => rule[0] === "my-class",
+  )?.[1];
+
+  if (!myClassDeclarations) {
+    throw new Error("No rule found for .my-class");
+  }
+
+  expect(myClassDeclarations).toStrictEqual(expected);
+});
