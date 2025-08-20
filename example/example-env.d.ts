19372e8ed05036f170a0a83543e58acaca15e36d
Mark Lawlor <mwlawlor@gmail.com>
2025-08-21 00:48:44 +1000

    feat: upgrade to SDK 54 preview (#107)

--- example/example-env.d.ts
+++ example/example-env.d.ts
@@ -1,2 +1,57 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../types.d.ts" />
+// This file is should be auto generated, you do not need to use this file
+
+export * from "react-native";
+
+declare module "react-native" {
+  interface ScrollViewProps
+    extends ViewProps,
+      ScrollViewPropsIOS,
+      ScrollViewPropsAndroid,
+      Touchable {
+    contentContainerClassName?: string;
+    indicatorClassName?: string;
+  }
+  interface FlatListProps<ItemT> extends VirtualizedListProps<ItemT> {
+    columnWrapperClassName?: string;
+  }
+  interface ImageBackgroundProps extends ImagePropsBase {
+    imageClassName?: string;
+  }
+  interface ImagePropsBase {
+    className?: string;
+    cssInterop?: boolean;
+  }
+  interface ViewProps {
+    className?: string;
+    cssInterop?: boolean;
+  }
+  interface TextInputProps {
+    placeholderClassName?: string;
+  }
+  interface TextProps {
+    className?: string;
+    cssInterop?: boolean;
+  }
+  interface SwitchProps {
+    className?: string;
+    cssInterop?: boolean;
+  }
+  interface InputAccessoryViewProps {
+    className?: string;
+    cssInterop?: boolean;
+  }
+  interface TouchableWithoutFeedbackProps {
+    className?: string;
+    cssInterop?: boolean;
+  }
+  interface StatusBarProps {
+    className?: string;
+    cssInterop?: boolean;
+  }
+  interface KeyboardAvoidingViewProps extends ViewProps {
+    contentContainerClassName?: string;
+  }
+  interface ModalBaseProps {
+    presentationClassName?: string;
+  }
+}
