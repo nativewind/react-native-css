460df7bc1c05d571cceccaf67ce7d31ec69a914f
Mark Lawlor <mwlawlor@gmail.com>
2025-10-01 23:47:48 +1000

    chore: cleanup example global.css

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,7 +7,9 @@ import "../global.css";
 export default function App() {
   return (
     <View className="flex-1 bg-white items-center justify-center">
-      <Text className="text-red-800 animate-bounce">Hello world!!!</Text>
+      <Text className="text-red-800 text-2xl font-bold animate-bounce">
+        Hello world!!!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
